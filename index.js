85908117616e7a85760055f995f4bffab75c59f0
Abhijit Patel <abhijitdev9@gmail.com>
2020-02-19 00:06:03 +0530

    [server] mongo setup
    MongoDb atlas used for database
    mongoose for connecting the database

--- index.js
+++ index.js
@@ -2,13 +2,35 @@ const express = require("express");
 const webpack = require("webpack");
 const webpackMiddleWare = require("webpack-dev-middleware");
 const history = require('connect-history-api-fallback');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 
 const webpackConfig = require("./webpack.config");
+const models = require('./server/models');
+const Blogs = mongoose.model('blogs');
+const MONGO_URI = 'mongodb+srv://dbUser:********@cluster0-5dv6u.mongodb.net/blogDb?retryWrites=true&w=majority';
 
+if (!MONGO_URI) {
+  throw new Error("You must provide a MongoLab URI");
+}
 const app = express();
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+mongoose.connection
+  .once("open", () => console.log("Connected to MongoLab instance."))
+  .on("error", error => console.log("Error connecting to MongoLab:", error));
+
+app.use(bodyParser.json());
+
+// app.get('/addBlog', (req, res) => { test for adding blog
+//   console.log('adding new blog');
+//   (new Blogs({ title: 'Blog 1', content:'title for first blog' })).save();
+//   res.send('added blog succesfully');
+// });
+
 app.use(history());
 
-app.use(webpackMiddleWare(webpack(webpackConfig)));
+// app.use(webpackMiddleWare(webpack(webpackConfig)));
 
 app.listen(8000, () => {
   console.log("Listening");
---
+++ server/models/blogs.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BlogSchema = new Schema({
+  title: { type: String },
+  content: { type: String },
+  createdOn: { type: Date },
+});
+
+BlogSchema.static.findBlog = function(id) {
+  const Blogs = mongoose.model('blogs');
+
+  return Blogs.findById(id);
+}
+
+mongoose.model('blogs', BlogSchema);
