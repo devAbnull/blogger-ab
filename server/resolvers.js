6c37450bd0c0b8eb5c84693faefd2ad5b6dfd601
Abhijit Patel <abhijitdev9@gmail.com>
2020-03-09 04:33:33 +0530

    [server] sorting of blog list fixed
    added support for scalar type Date for createOn field in blog
    sorted list in descending order of createdOn field while querying blogs

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { GraphQLScalarType } = require('graphql');
 require('./models');
 const Blogs = mongoose.model('blogs');
 
@@ -12,9 +13,20 @@ function adaptBlogToSave({ title, content, createdOn }) {
 }
 
 const resolvers = {
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    parseValue(value) {
+      return new Date(value); // value from the client
+    },
+    serialize(value) {
+      return value.getTime(); // value sent to the client
+    },
+  }),
+
   Query: {
-    blogs: async () => await Blogs.find({}),
-    blog: async (root, { id, ...args}) => {
+    blogs: async () => await Blogs.find({}).sort({ createdOn: -1 }),
+    blog: async (root, { id, ...args }) => {
       if (id) {
         return await Blogs.findById(id);
       }
@@ -25,7 +37,11 @@ const resolvers = {
   Mutation: {
     addBlog: async (root, args) => await (new Blogs(adaptBlogToSave(args))).save(),
     deleteBlog: async (root, { id }) => await Blogs.findOneAndDelete({ _id: id }),
-  }
+    updateBlog: async (root, { id, ...args }) => await Blogs.findOneAndUpdate(
+      { _id: id },
+      { $set: args },
+      { new: true }),
+  },
 };
 
 module.exports = resolvers;
--- server/schema.js
+++ server/schema.js
@@ -1,22 +1,25 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  scalar Date
+
   type Blog {
     id: String
     title: String
     content: String
     summary: String
-    createdOn: Int
+    createdOn: Date
   }
 
   type Query {
     blogs: [Blog]
-    blog(id: String, title: String, createdOn: Int) : Blog
+    blog(id: String, title: String, createdOn: Date) : Blog
   }
 
   type Mutation {
-    addBlog(title: String, createdOn: Int, content: String): Blog
+    addBlog(title: String, createdOn: Date, content: String): Blog
     deleteBlog(id: String): Blog
+    updateBlog(id: String, title: String, createdOn: Date, content: String): Blog
   }
 `;
 
